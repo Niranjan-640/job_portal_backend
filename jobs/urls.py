3573f068fcc70338c128e3977f125c318903af7d
niranjan <nniranjan640@gmail.com>
2025-11-07 10:25:56 +0530

    delete all

--- jobs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CandidateCreateAPIView
-
-
-urlpatterns = [
-    path('api/candidates/', CandidateCreateAPIView.as_view(), name='candidate-create'),
-]
